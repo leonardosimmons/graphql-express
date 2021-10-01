211c08fdf7f8c5cdd1910e1c9e0e1073dc1afc74
Leonardo Simmons <leonardo.simmons07@gmail.com>
2021-10-01 13:08:11 -0700

    base set up

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+import { ApolloServer } from "apollo-server";
+import { context } from "../graphql/context";
+import { schema } from "../graphql/schema";
+
+const server = new ApolloServer({
+  schema: schema,
+  context: context,
+});
+
+server.listen().then(async ({ url }) => {
+  console.log(`\
+🚀 Server ready at: ${url}
+  `);
+});
